93f204f84dbdc9bad448d610b9e2f4284d38fc86
Jander Laffita <janderdeveloper@gmail.com>
2023-07-01 07:13:26 -0400

    add movie api model

---
+++ lib/src/data/models/moviedb_response.dart
@@ -0,0 +1,33 @@
+import 'movie_api_model.dart';
+
+class MovieDbResponse {
+    
+    final int page;
+    final List<MovieMovieDB> results;
+    final int totalPages;
+    final int totalResults;
+
+    MovieDbResponse({
+        required this.page,
+        required this.results,
+        required this.totalPages,
+        required this.totalResults,
+    });
+
+    factory MovieDbResponse.fromJson(Map<String, dynamic> json) => MovieDbResponse(
+        page: json["page"],
+        results: List<MovieMovieDB>.from(json["results"].map((x) => MovieMovieDB.fromJson(x))),
+        totalPages: json["total_pages"],
+        totalResults: json["total_results"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "page": page,
+        "results": List<dynamic>.from(results.map((x) => x.toJson())),
+        "total_pages": totalPages,
+        "total_results": totalResults,
+    };
+}
+
+
+
